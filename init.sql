295990e7c19374314f005da53b837b19abbe42a9
Manyim <manyimparis@gmail.com>
2023-06-01 14:33:58 +0200

    maj

--- init.sql
+++ init.sql
@@ -1,14 +1,15 @@
 CREATE TABLE joueurs(j_id SERIAL PRIMARY KEY, 
 nom VARCHAR(50) NOT NULL, 
 age INTEGER NOT NULL, 
-nom_equipe_nationale VARCHAR(50) NOT NULL, 
-nom_championnat VARCHAR(50) NOT NULL, 
+equipe_nationale VARCHAR(50) NOT NULL, 
+championnat VARCHAR(50) NOT NULL, 
 poste VARCHAR(50) NOT NULL);
 
 CREATE TABLE questions (q_id SERIAL PRIMARY KEY, question VARCHAR(150) NOT NULL);
-INSERT INTO questions(question,liaison) VALUES ('le joueur est-il d''origine ','origine');
+INSERT INTO questions(question,liaison) VALUES ('le joueur est-il d''origine ','equipe_nationale');
 INSERT INTO questions(question,liaison) VALUES ('le joueur a-il plus de ','age');
 INSERT INTO questions(question,liaison) VALUES ('le joueur a-il moins de ','age');
 INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il dans l''équipe nationale ','equipe_nationale');
-INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il dans un championnat ','championnat');
+INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il dans un ','championnat');
 INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il au poste ','poste');
+
--- server.js
+++ server.js
@@ -6,12 +6,35 @@ app.set('view engine', 'ejs');
 const PORT = 3000;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // Middleware pour parser les données du corps de la requête
 app.get('/', (req, res) => {
     res.render('Chatbot.ejs', { showButtons: false });
 });
 
-app.post('/', (req, res) => {
-    res.render('Chatbot.ejs', { showButtons: true });
+app.post('/', async (req, res) => {
+    if (req.body.commencer) {
+        try {
+            const result = await pool.query('SELECT * FROM questions ORDER BY RANDOM() LIMIT 1');
+            const liaison = result.rows[0].liaison;
+            const question = result.rows[0].question;
+            const resultLiaison = await pool.query(`SELECT ${liaison} FROM joueurs ORDER BY RANDOM() LIMIT 1`);
+            const joueur = resultLiaison.rows[0][liaison];
+            let full = "";
+            if (liaison == "age") {
+                full = "ans?";
+            }
+            else {
+                full = "?";
+            }
+            const resultat = question + joueur + full;
+            res.render('Chatbot.ejs', { showButtons: true, question: resultat });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send("Erreur lors de la récupération des données");
+        }
+    } else {
+        res.render('Chatbot.ejs', { showButtons: false });
+    }
 });
 
 app.listen(PORT, () => {
