7a32140d355d3f893953186c0b83fe30cb5be491
MANYIM olivier <manyimparis@gmail.com>
2023-06-02 23:45:46 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,5 +6,6 @@ championnat VARCHAR(50) NOT NULL,
 poste VARCHAR(50) NOT NULL);
 
 CREATE TABLE questions (q_id SERIAL PRIMARY KEY, question VARCHAR(150) NOT NULL);
-
+\copy joueurs from 'joueurs.csv' WITH (FORMAT CSV, HEADER);
+\copy questions from 'questions.csv' WITH (FORMAT CSV, HEADER);
 
