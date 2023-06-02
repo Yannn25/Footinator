e55b357315c937f421c15820fc5f7995256b136f
MANYIM olivier <manyimparis@gmail.com>
2023-06-02 23:30:10 +0200

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,10 +6,5 @@ championnat VARCHAR(50) NOT NULL,
 poste VARCHAR(50) NOT NULL);
 
 CREATE TABLE questions (q_id SERIAL PRIMARY KEY, question VARCHAR(150) NOT NULL);
-INSERT INTO questions(question,liaison) VALUES ('le joueur est-il d''origine ','equipe_nationale');
-INSERT INTO questions(question,liaison) VALUES ('le joueur a-il plus de ','age');
-INSERT INTO questions(question,liaison) VALUES ('le joueur a-il moins de ','age');
-INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il dans l''équipe nationale ','equipe_nationale');
-INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il dans un ','championnat');
-INSERT INTO questions(question,liaison) VALUES ('le joueur joue t''il au poste ','poste');
+
 
