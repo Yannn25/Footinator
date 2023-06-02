47e5bebfa03a2de90971a0172e444d6fae05355a
MANYIM olivier <manyimparis@gmail.com>
2023-06-03 00:06:19 +0200

    Delete questions.js

--- questions.js
+++
@@ -1,8 +0,0 @@
-const { pool } = require('./database');
-
-async function getQuestions() {
-    const res = await pool.query('SELECT * FROM questions');
-    return res.rows;
-}
-
-module.exports = { getQuestions };
